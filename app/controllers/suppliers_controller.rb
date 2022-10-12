11cafbd297093a5663f4134b065af2658bd5ccfc
mercy.waeni <mercy.waeni@calltronix.com>
2022-10-13 00:01:46 +0300

    purchase items

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,16 @@
 class SuppliersController < ApplicationController
+    def index
+        # @suppliers = Supplier.all 
+    end
+
+    def create
+        @supplier = Supplier.create!(supplier_params)
+    end
+
+
+
+    private
+    def supplier_params
+        params.permit(:supplier_ref, :supplier_name, :contact, :paybill_number)
+    end
 end
